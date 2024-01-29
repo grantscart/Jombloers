45af8ec016352705fdf5de02c7f93ccf5e7404f7
Grant <80586800+grantscart@users.noreply.github.com>
2024-01-29 10:11:28 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   prefix: 'd?',
   owner: ["715783278237450280", "937998965754986526"],
-  token: process.env.token
+  token: process.env.token,
+  ERR_WEB: process.env.webhk
 };
